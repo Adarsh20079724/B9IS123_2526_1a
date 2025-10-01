1d82bfb3a8d29bcb83277ef7570aebb093300e82
Adarsh20079724 <20079724@mydbs.ie>
2025-10-01 10:11:05 +0100

    Euler 1 code updated

--- ex2.js
+++ ex2.js
@@ -1,3 +1,17 @@
-let euler1=()=>alert();
+
+const randomNumber = 10
+
+
+
+let euler1 = (num) => {
+    let sum = 0;
+
+    for(let i = 1; i < num ; i++){
+        if(i%3 == 0 || i%5 == 0){
+            sum += i;
+        }
+    }
+   return alert(" Sum is: ", sum);
+}
 
 let eulerCustom=()=>alert()
