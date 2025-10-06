e6303495790ad3abc1bad0033d3157106bb2637d
Adarsh20079724 <20079724@mydbs.ie>
2025-10-06 10:22:18 +0100

    Sample code written.

--- ex4.js
+++ ex4.js
@@ -11,6 +11,27 @@
 // Example Output : 
 // 23 : 3 5 : 1 2 3 4 5 6 7 8 9
 
+const inputStr = "35:123456789";
+
+const euler1 = (str) => {
+    
+    let sum = 0, output = [];
+    let [div, range] = str.split(":")
+    console.log("multiples : ",div)
+    const multiples = div.split("")
+    
+    for(let i = 1; i <= range.split("").findLast(x => x) ; i++){
+        if(i%multiples[0] == 0 || i%multiples[1] == 0){
+            sum += i;
+        }
+    }
+    
+    output = [sum, div, range];
+  return console.log("output string = ", output.join(":"))
+}
+
+euler1(inputStr) ;
+
 //keep code DRY (Don't repeat yourself)
 
 //if you're finished, code it into a page
